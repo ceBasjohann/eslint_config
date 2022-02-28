ab28de5c37438236eef74f466c1d851989340e41
JohannSB <johann.bitencourt@somossimpay.com.br>
2022-02-28 02:41:57 -0300

    test

---
+++ sla.js
@@ -0,0 +1,14 @@
+let x = 5
+let y = 2
+let z = x + y;  
+
+function test(){
+      console.log(  "test ")
+
+      document.getElementById('demo').innerHTML = Date()
+     
+        };
+
+        if(){
+            else({})
+        }
